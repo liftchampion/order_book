0ca985e90335a2af48b94ea7219311dd43d98a33
ggerardy <liftchampion@gmail.com>
2019-09-09 14:20:22 +0300

    Benchmark template added

--- main.cpp
+++ main.cpp
@@ -1,81 +1,44 @@
 #include <iostream>
 #include "offers_list.h"
-
-#include <map>
+#include "profile.h"
+#include <limits>
+#include <random>
+#include <string>
 
 using namespace std;
 
-int main()
+void benchmark(
+				int int_part_min,
+				int int_part_max,
+				int decimal_part_min,
+				int decimal_part_max,
+				int amount_min,
+				int amount_max,
+				int tests_count,
+				string msg
+			   ) {
+	auto	seed = 42;
+	mt19937 gen(seed);
+	uniform_int_distribution<int> dist_int_part(int_part_min,int_part_max);
+	uniform_int_distribution<int> dist_decimal_part(decimal_part_min,decimal_part_max);
+	uniform_int_distribution<int> dist_amount(amount_min,amount_max);
+
+
+	{
+		LOG_DURATION(msg)
+		OffersList offers_list;
+		for (int i = 0; i < tests_count; ++i){
+			offers_list[dist_int_part(gen) +
+						static_cast<double>(dist_decimal_part(gen)) /
+						decimal_part_max] = dist_amount(gen);
+		}
+		cout << offers_list << endl;
+	}
+}
+
+
+int		main()
 {
-
-	map<int, string> qq;
-
-	auto ss = qq.begin();
-
-	OffersList offers_list;
-//	cout << offers_list << endl;
-
-	offers_list[61.20] = 20000;
-
-	offers_list[64.4] = 1;
-
-	auto gg = offers_list.begin();
-	++gg;
-
-	gg->amount = 101;
-
-	offers_list[1.45] = 1;
-
-	offers_list[1000.457] = 99;
-
-	offers_list[1.45] = 0;
-	offers_list[1000.457] = 0;
-
-	offers_list[0] = 7;
-	offers_list[61.20] = 0;
-	offers_list[0] = 0;
-	offers_list[64.4] = 0;
-
-	offers_list.rebuild();
-
-	//offers_list[-1] = 0;
-
-	offers_list[61.20] = 201;
-	offers_list[64.42] = 1;
-	offers_list[50] = 11;
-
-	offers_list[121.2] = 33663;
-	offers_list[131.2] = 33993;
-	offers_list[100.2] = 331243;
-
-
-	cout << offers_list << endl << endl;
-
-
-	auto ff = offers_list.find(100.2);
-
-	ff->amount = 8;
-
-	if (offers_list.find(1337) != offers_list.end())
-		offers_list.at(1337) = 100;
-
-
-	cout << offers_list << endl << "Check reverse" << endl;
-
-	for (auto it = offers_list.rbegin(); it != offers_list.rend(); ++it) {
-		cout << it->price << endl;
-	}
-	cout << endl;
-
-	auto before_end = offers_list.end();
-	before_end--;
-	auto before_begin = offers_list.begin();
-	before_begin--;
-	for (auto it = before_end; it != before_begin; --it) {
-		cout << it->price << endl;
-	}
-	cout << endl;
-
-
+	benchmark(0,100,0,10,0,1000, 1000, "hui");
 	return 0;
 }
